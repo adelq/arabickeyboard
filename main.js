dab338ce538088ea60c108a65586eeff533190cb
Adel Qalieh <aqalieh95@gmail.com>
2017-11-24 09:46:03 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+TABLE = {
+  "2": "ء",
+  "3": "ع",
+  "7": "ح"
+}
+
+var translate = function(input) {
+  var output = "";
+  for (var i = 0, len = input.length; i < len; i++) {
+    if (input[i] in TABLE) {
+      output += TABLE[input[i]];
+    } else {
+      output += input[i];
+    }
+  }
+  return output;
+}
+
+var root = document.body;
+
+var state = {
+  text: "Write in 3araby and see the result immediately below",
+  update: function(value) {
+    state.text = value;
+  }
+}
+
+var Editor = {
+  view: function() {
+    return [
+      m("textarea.input", {
+        oninput: m.withAttr("value", state.update),
+        value: state.text
+      }),
+      m(".preview", translate(state.text))
+    ]
+  }
+}
+
+m.mount(document.getElementById("editor"), Editor)
